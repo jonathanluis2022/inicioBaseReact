64c872f41b9947bc1a57379d8fabd2b072b880c2
function <jonathan.luisrodrigues@hotmail.com>
2022-11-22 19:31:14 -0300

    inicioReact

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
 import logo from './logo.svg';
 import './App.css';
+import{ Component } from 'react';
 
-function App() {
+class App extends Component {
+constructor(props) {
+  super(props) 
+ 
+
+  this.state = {
+    name: 'Jonathan',
+    counter : 0
+  }
+}
+
+hanldePClick = () => {
+  this.setState({name : 'Luis'})
+}
+
+handleAClick = (event) => {
+  event.preventDefault();
+  const { counter } = this.state
+  this.setState({counter : counter + 1})
+  
+}
+
+render() {
+
+  const  { name , counter } = this.state;
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+        <p onClick={this.hanldePClick}>
+          {name} {counter}
         </p>
+        
         <a
+          onClick={this.handleAClick}
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          não vai para o linck
         </a>
       </header>
     </div>
   );
+}
+
 }
 
 export default App;
